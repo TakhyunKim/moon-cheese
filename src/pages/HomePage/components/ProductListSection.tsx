025541bbefe295944ece732c98c7e7355c831b7f
TakhyunKim <kimkih1218@gmail.com>
2025-12-07 17:44:58 +0900

    feat: add price format feature to product card

--- src/pages/HomePage/components/ProductListSection.tsx
+++ src/pages/HomePage/components/ProductListSection.tsx
@@ -2,8 +2,12 @@ import { Counter, SubGNB, Text, AsyncBoundary } from '@/ui-lib';
 import { useState } from 'react';
 import { useNavigate } from 'react-router';
 import { Box, Grid, styled } from 'styled-system/jsx';
-import ProductItem from '../components/ProductItem';
+import { useAtomValue } from 'jotai';
+import { currencyAtom } from '@/shared/atoms/currency';
+import { useExchangeRateOfCurrency } from '@/shared/hooks/useCurrency';
+import { formatPriceWithCurrency } from '@/utils/currency';
 
+import ProductItem from '../components/ProductItem';
 import { getProductList, type Product } from '../api/product';
 
 import type { UseSuspenseQueryOptions } from '@tanstack/react-query';
@@ -12,12 +16,6 @@ type Category = 'all' | 'CHEESE' | 'CRACKER' | 'TEA';
 
 function ProductListSection() {
   const [currentTab, setCurrentTab] = useState<Category>('all');
-
-  const navigate = useNavigate();
-
-  const handleClickProduct = (productId: number) => {
-    navigate(`/product/${productId}`);
-  };
 
   return (
     <styled.section bg="background.01_white">
@@ -34,28 +32,40 @@ function ProductListSection() {
       </SubGNB.Root>
       <Grid gridTemplateColumns="repeat(2, 1fr)" rowGap={9} columnGap={4} p={5}>
         <AsyncBoundary {...productListQueryOptions(currentTab)}>
-          {products =>
-            products.map(product => (
-              <ProductItem.Root key={product.id} onClick={() => handleClickProduct(product.id)}>
-                <ProductItem.Image src={product.images[0]} alt={product.name} />
-                <ProductItem.Info title={product.name} description={product.description} />
-                <ProductItem.Meta>
-                  <ProductItem.MetaLeft>
-                    <ProductItem.Rating rating={product.rating} />
-                    <ProductItem.Price>{product.price}</ProductItem.Price>
-                  </ProductItem.MetaLeft>
-                </ProductItem.Meta>
-                <Counter.Root>
-                  <Counter.Minus onClick={() => {}} disabled={true} />
-                  <Counter.Display value={1} />
-                  <Counter.Plus onClick={() => {}} />
-                </Counter.Root>
-              </ProductItem.Root>
-            ))
-          }
+          {products => products.map(product => <ProductCard key={product.id} product={product} />)}
         </AsyncBoundary>
       </Grid>
     </styled.section>
+  );
+}
+
+function ProductCard({ product }: { product: Product }) {
+  const currency = useAtomValue(currencyAtom);
+  const exchangeRate = useExchangeRateOfCurrency({ price: product.price });
+  const formattedPrice = formatPriceWithCurrency({ price: exchangeRate, currency });
+
+  const navigate = useNavigate();
+
+  const handleClickProduct = (productId: number) => {
+    navigate(`/product/${productId}`);
+  };
+
+  return (
+    <ProductItem.Root key={product.id} onClick={() => handleClickProduct(product.id)}>
+      <ProductItem.Image src={product.images[0]} alt={product.name} />
+      <ProductItem.Info title={product.name} description={product.description} />
+      <ProductItem.Meta>
+        <ProductItem.MetaLeft>
+          <ProductItem.Rating rating={product.rating} />
+          <ProductItem.Price>{formattedPrice}</ProductItem.Price>
+        </ProductItem.MetaLeft>
+      </ProductItem.Meta>
+      <Counter.Root>
+        <Counter.Minus onClick={() => {}} disabled={true} />
+        <Counter.Display value={1} />
+        <Counter.Plus onClick={() => {}} />
+      </Counter.Root>
+    </ProductItem.Root>
   );
 }
 
